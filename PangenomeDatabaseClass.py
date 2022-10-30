424c78c67d478ea1eb70ca5007d7956b4e643c4d
Robin <stragierrobin@gmail.com>
2022-10-30 18:29:35 +0100

    Vaticle Pangenome Database Class

--- PangenomeDatabaseClass.py
+++ PangenomeDatabaseClass.py
@@ -1,6 +1,7 @@
 from typedb.client import TypeDB, SessionType, TransactionType
 from typedb.common.exception import TypeDBClientException
 from subprocess import Popen
+import ijson
 
 
 class PangenomeDatabase:
@@ -34,35 +35,68 @@ class PangenomeDatabase:
 
     def delete(self):
         # delete the database
-        self.client.databases().get(self.name).delete()
+        try:
+            self.client.databases().get(self.name).delete()
+        except TypeDBClientException:
+            print("Database does not exist...")
 
-    def create(self, replace=False):
+    def create(self, replace=False, file: str = "./Data/schema.tql"):
 
         if replace is True:
             print("(Re-)Creating Database")
-            try:
-                self.delete()
-            except TypeDBClientException:
-                pass
-            finally:
-                self.client.databases().create(self.name)
-                with open("./Data/schema.tql", "r") as schema:
-                    query = schema.read().replace("\n", "")
-                with self.client.session(self.name, SessionType.SCHEMA) as session:
-                    with session.transaction(TransactionType.WRITE) as transaction:
-                        transaction.query().define(query)
-                        transaction.commit()
+            self.delete()
+            self.client.databases().create(self.name)
+            with open(file, "r") as schema:
+                query = schema.read().replace("\n", "")
+            with self.client.session(self.name, SessionType.SCHEMA) as session:
+                with session.transaction(TransactionType.WRITE) as transaction:
+                    transaction.query().define(query)
+                    transaction.commit()
         elif not self.exists():
             self.create(replace=True)
         else:
             print(f"There already exists a database called {self.name}.")
 
-    def migrate(self):
-        pass
+    def migrate(self, file, template):
+        with open(file, "r") as file:
+            data = [template(item) for item in ijson.items(file, "item")]
 
-    def query(self, query: str):
-        pass
+        with self.client.session(self.name, SessionType.DATA) as session:
+            with session.transaction(TransactionType.WRITE) as transaction:
+                for insert in data:
+                    print(insert)
+                    transaction.query().insert(insert)
+                transaction.commit()
+
+    def gene_template(self, input):
+        insert = f'insert $gene isa Gene, '
+        for key, value in input.items():
+            try:
+                value = int(value)
+                insert += f'has {key} {value}, '
+            except ValueError:
+                insert += f'has {key} "{value}", '
+
+        return insert[:-2] + ";"
+
+    def query(self, query: str = "./Data/getGeneNames.tql"):
+        with self.client.session(self.name, SessionType.DATA) as session:
+            with session.transaction(TransactionType.READ) as transaction:
+                with open(query, 'r') as query:
+                    query = query.read().replace("\n", "")
+
+                iterator = transaction.query().match(query)
+                get = query.split("$")[-1].rstrip(";")
+
+                results = [res.get(get).get_value() for res in iterator]
+
+        return results
+
 
 if __name__ == "__main__":
-    with PangenomeDatabase("Spidermite") as Spidermite:
-        print(Spidermite.name)
+    with PangenomeDatabase("Spidermite") as Db:
+        Db.create(replace=True)
+        Db.migrate("./Data/Genes.json", Db.gene_template)
+        for result in Db.query():
+            print(result)
+        Db.delete()
