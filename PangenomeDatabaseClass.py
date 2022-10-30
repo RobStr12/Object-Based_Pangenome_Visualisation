19d01119744e05f0de0e251a27c5e14359159b5c
Robin <robin.stragier@telenet.be>
2022-10-30 16:56:20 +0100

    Vaticle Database

--- PangenomeDatabaseClass.py
+++ PangenomeDatabaseClass.py
@@ -12,26 +12,34 @@ class PangenomeDatabase:
         self.client = None
 
     def __enter__(self):
-        self.server = Popen("server.bat")
-        self.client = TypeDB.core_client("localhost:1730")
+        self.start()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def start(self):
+        # Start the server at localhost:1730 & start the client
+        self.server = Popen("server.bat")
+        self.client = TypeDB.core_client("localhost:1730")
+
+    def close(self):
+        # close the client & terminate the server
         self.client.close()
         self.server.terminate()
 
     def exists(self):
-        return self.client.databases().contains("self.name")
+        # check if the database exists
+        return self.client.databases().contains(self.name)
 
     def delete(self):
+        # delete the database
         self.client.databases().get(self.name).delete()
 
-    def create(self, replace: bool = True):
+    def create(self, replace=False):
 
         if replace is True:
-
             print("(Re-)Creating Database")
-
             try:
                 self.delete()
             except TypeDBClientException:
@@ -44,6 +52,10 @@ class PangenomeDatabase:
                     with session.transaction(TransactionType.WRITE) as transaction:
                         transaction.query().define(query)
                         transaction.commit()
+        elif not self.exists():
+            self.create(replace=True)
+        else:
+            print(f"There already exists a database called {self.name}.")
 
     def migrate(self):
         pass
@@ -52,5 +64,5 @@ class PangenomeDatabase:
         pass
 
 if __name__ == "__main__":
-    with PangenomeDatabase("Spidermite") as PDB:
-        PDB.delete()
+    with PangenomeDatabase("Spidermite") as Spidermite:
+        print(Spidermite.name)
